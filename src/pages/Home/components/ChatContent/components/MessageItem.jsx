05a6ffb342256bade754a8a7bdac40d2ba731897
Tang Bao Hung <baohung20020112@gmail.com>
2023-12-02 11:10:16 +0700

    Update message ui

--- src/pages/Home/components/ChatContent/components/MessageItem.jsx
+++ src/pages/Home/components/ChatContent/components/MessageItem.jsx
@@ -6,14 +6,29 @@ export default function MessageItem({message}) {
     const {currentUser} = useSelector(state => state.user);
 
     const isUserMessage = () => {
-        return message.sender === currentUser._id
+        return message.sender._id === currentUser._id
     }
-
-    const {content, createdAt} = message
+console.log(message)
+    const {content, createdAt, sender} = message
   return (
-    <div className={`flex flex-col gap-1 p-2 rounded-2xl w-fit max-w-[450px] ${isUserMessage() ? 'bg-primary text-white self-end': 'bg-gray-200 '}`}>
-        <span className='pr-8'>{content}</span>
-        <span className='text-right text-xs'>{moment(createdAt).format('HH:mm')}</span>
+    // <div className={`flex flex-col gap-1 p-2 rounded-2xl w-fit max-w-[450px] ${isUserMessage() ? 'bg-primary text-white self-end': 'bg-gray-200 '}`}>
+    //     <span className='pr-8'>{content}</span>
+    //     <span className='text-right text-xs'>{moment(createdAt).format('HH:mm')}</span>
+    // </div>
+    <div className={`chat ${isUserMessage() ? 'chat-end' : 'chat-start'}`}>
+     {!isUserMessage() ? <div className='chat-image avatar'>
+        <div className='w-10 rounded-full'>
+          <img src={sender.avatar}/>
+        </div>
+      </div> : <></>}
+
+      <div className='chat-header'>
+        {sender?.name} 
+      </div>
+      <div className={`chat-bubble ${isUserMessage() ? 'chat-bubble-primary' : ''}`}>{content}</div>
+      <div className='chat-footer'>
+        <time className='text-xs opacity-50'>{moment(createdAt).format("HH:mm")}</time>
+      </div>
     </div>
   )
 }
